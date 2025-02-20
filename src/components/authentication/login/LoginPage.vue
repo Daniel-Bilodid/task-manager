12221f7c57014044352cd0df64193d4b7db68687
Daniel <danielbeloded@gmail.com>
2025-02-20 15:05:23 +0200

    working on project

---
+++ src/components/authentication/login/LoginPage.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="login">
+    <div class="login__wrapper">
+      <img class="login__img" :src="logo" alt="Logo" />
+
+      <h1 class="login__title">Login To Your Dashboard</h1>
+
+      <div class="login__inputs">
+        <p>Email</p>
+        <input placeholder="Input yout email" class="login__input" type="text" />
+
+        <p>Password</p>
+        <input placeholder="Input yout password" class="login__input" type="text" />
+      </div>
+
+      <button class="login__btn">Login</button>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import './loginPage.scss'
+import logo from '@/assets/logo.svg'
+
+export default {
+  name: 'LoginPage',
+  data() {
+    return {
+      logo,
+    }
+  },
+}
+</script>
---
+++ src/components/nav/NavPage.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="nav">
+    <div class="nav__wrapper">
+      <img class="nav__img" :src="logo" alt="Logo" />
+      <div class="nav__title">taski</div>
+    </div>
+
+    <ul class="nav__list">
+      <li class="nav__list-item">
+        <img :src="category" alt="category" />
+        <p class="nav__list-title">Overview</p>
+      </li>
+      <li class="nav__list-item">
+        <img :src="book" alt="book" />
+        <p class="nav__list-title">Task</p>
+      </li>
+      <li class="nav__list-item">
+        <img :src="mentor" alt="mentor" />
+        <p class="nav__list-title">Mentors</p>
+      </li>
+      <li class="nav__list-item">
+        <img :src="message" alt="mewssage" />
+        <p class="nav__list-title">Message</p>
+      </li>
+      <li class="nav__list-item">
+        <img :src="settings" alt="settings" />
+        <p class="nav__list-title">Settings</p>
+      </li>
+    </ul>
+
+    <div class="nav__help">
+      <div class="nav__help-title">Help Center</div>
+
+      <div class="nav__help-descr">
+        Having Trouble in Learning. Please contact us for more questions.
+      </div>
+
+      <div class="nav__help-btn">Go To Help Center</div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import './navPage.scss'
+import logo from '@/assets/logo.svg'
+import category from '../icons/navIcons/category.svg'
+import book from '../icons/navIcons/book.svg'
+import mentor from '../icons/navIcons/octagon.svg'
+import message from '../icons/navIcons/message.svg'
+import settings from '../icons/navIcons/setting.svg'
+
+export default {
+  name: 'NavPage',
+  data() {
+    return {
+      logo,
+      category,
+      book,
+      mentor,
+      settings,
+      message,
+    }
+  },
+}
+</script>
